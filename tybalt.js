e8e3e00ed02922371a1de42466cbd57a9d3e54e3
Nick Carpenetti <nickcarpenetti@gmail.com>
2016-05-20 00:26:28 -0700

    a few changes for recognizing commands

--- tybalt.js
+++ tybalt.js
@@ -4,10 +4,40 @@ var Tybalt = new Discord.Client();
 
 Tybalt.on("message", function(message)
 {
-  if(message.content === "Hi")
+  console.log(message.channel);
+  if(message.author.username != "Tybalt Leftpaw")
   {
-    Tybalt.reply(message, "Hey there!");
+    Tybalt.reply(message, message.content);
   }
-});
+
+  if(typeof(message.channel) === "PMChannel")
+  {
+    Tybalt.reply(message, "PMChannel");
+    if(memberHasRole(message.author.username, "Leader") && message.content === "goodnight")
+    {
+      Tybalt.reply(message, "Goodnight, " + message.author.username);
+      //Tybalt.logout();
+    }
+    else
+    {
+      Tybalt.reply(message, "not a leader");
+    }
+
+  }
+  else if(typeof(message.channel) === "TextChannel")
+  {
+    Tybalt.reply(message, "TextChannel");
+    if(message.content === "Hi")
+    {
+      Tybalt.reply(message, "Hey there!");
+    }
+  }
+
+  if(message.content == "off")
+  {
+  Tybalt.logout();
+  }
+}
+);
 
 Tybalt.login("corleonisbot@gmail.com", "ApplesForSale");
